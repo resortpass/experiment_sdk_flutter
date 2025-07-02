dec4955df78ac3ae9de83c4ce1e91e873c68ca6c
Rodrigo Martins <rodrigolmti@gmail.com>
2025-07-02 10:45:54 -0500

    Update dependencies

--- lib/integrations/experiment_analytics.dart
+++ lib/integrations/experiment_analytics.dart
@@ -1,4 +1,6 @@
 import 'package:amplitude_flutter/amplitude.dart';
+import 'package:amplitude_flutter/configuration.dart';
+import 'package:amplitude_flutter/events/base_event.dart';
 import 'package:experiment_sdk_flutter/types/experiment_exposure_tracking_provider.dart';
 import 'package:experiment_sdk_flutter/types/experiment_variant.dart';
 
@@ -6,15 +8,17 @@ class AnalyticsExposureTrackingProvider
     implements ExperimentExposureTrackingProvider {
   @override
   Future<void> exposure(
-      String flagkey, ExperimentVariant? variant, String instanceName) async {
-    final properties = {'variant': variant?.value, 'flag_key': flagkey};
+    String flagKey,
+    ExperimentVariant? variant,
+    String instanceName,
+  ) async {
+    final properties = {'variant': variant?.value, 'flag_key': flagKey};
 
     if (variant == null) {
       properties.remove('variant');
     }
 
-    final ampli = Amplitude.getInstance(instanceName: instanceName);
-    await ampli.logEvent("\$exposure", eventProperties: properties);
-    await ampli.uploadEvents();
+    final amplitude = Amplitude(Configuration(apiKey: instanceName));
+    await amplitude.track(BaseEvent("\$exposure", eventProperties: properties));
   }
 }
